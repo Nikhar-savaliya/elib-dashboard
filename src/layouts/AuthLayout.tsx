95fc6bfc9a956c7020669571ff2bf177ca55bf42
Nikhar Savaliya <127284370+Nikhar-savaliya@users.noreply.github.com>
2024-04-27 19:47:37 +0530

    make route protected

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router-dom";
+import useUserStore from "@/store";
+import { Navigate, Outlet } from "react-router-dom";
 
 type Props = {};
 
 const AuthLayout = (props: Props) => {
+  const token = useUserStore((state) => state.token);
+
+  if (token) {
+    return <Navigate to={"/dashboard/home"} replace />;
+  }
+
   return (
     <>
       <Outlet />
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -26,11 +26,18 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
+import useUserStore from "@/store";
 
 type Props = {};
 
 const DashboardLayout = (props: Props) => {
+  const token = useUserStore((state) => state.token);
+
+  if (!token) {
+    return <Navigate to={"/auth/login"} replace />;
+  }
+
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-card md:block">
